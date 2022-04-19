3a09a41ccfd2a96b88ca179c895488ebc2947d63
BesteSakar <bestesakarr@outlook.com>
2022-04-19 21:56:37 +0300

    ConsoleAttributeReflection
    Attr. ve Ref. ornek olarak ise bir öğrenci sınıfı oluşturup verilerin girilmesini zorunlu hale
    getirme.
    Eksik olması halinde false dönüş yapması..

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student student1 = new student()
+            {
+                name = "Beste",
+                surname = "Sakar",
+                student_number = 1234,
+                age = 22
+            };
+            Console.WriteLine(control.check(student1));
+            Console.ReadLine();
+           
+        }
+
+
+
+        public class student
+        {
+            [zorunlu]
+            public string name;
+            [zorunlu]
+            public string surname;
+            [zorunlu]
+            public int student_number;
+            [zorunlu]
+            public int age;
+        }
+
+        public class zorunluAttribute : Attribute
+        {
+
+        }
+
+        public static class control
+        {
+            public static bool check(student std)
+            {
+                Type type = std.GetType();
+                foreach(var item in type.GetFields())
+                {
+                    object[] attributes = item.GetCustomAttributes(typeof(zorunluAttribute), true);
+                    if(attributes.Length != 0)
+                    {
+                        object value = item.GetValue(std);
+                        if(value == null)
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+
+
+    }
+}
